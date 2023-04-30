58b99cd3cdec427ddf9815013ca9ea98a2f106b9
Daniel Custodio <play.daniel@hotmail.com>
2023-04-30 15:50:52 -0300

    Processamento de dados e casting

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,38 @@ public class Main {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		
-		int x = 1000000000;
+		int x,z;
+		double y, resultado;	
+		x = 5;
+		z = 2;
+		y = 2*x;
+		
+		resultado = (double) x/z;
+		
+		double b, B, h, area;
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b+B)/2.0*h;
+		
+		System.out.println(area);
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(resultado); //casting
+		/*int x = 1000000000;
 		double y = 10.35784;
 		String nome = "Maria";
 		int idade = 31;
 		double renda = 4000.0;
-		
 		System.out.println("Olá, Mundo!");
 		System.out.println(x);
 		System.out.println(y);
 		System.out.printf("%.2f%n", y);
 		System.out.println("Resultado: "+x+" quilômetros");
 		System.out.printf("Resultado: %.3f metros %n", y);
-		System.out.printf("%s tem %d anos e seu salário é de R$%.2f/por mês. %n", nome, idade, renda);
+		System.out.printf("%s tem %d anos e seu salário é de R$%.2f/por mês. %n", nome, idade, renda);*/
+		
 	}
 
 }
